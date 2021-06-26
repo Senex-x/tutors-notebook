14c2aab7cdf979f5057c830d2313d3233c93359a
V-Senex <junior.07.inator@gmail.com>
2021-06-26 17:12:51 +0300

    Student entity update

--- app/src/main/java/com/example/tutorsnotebook/entities/Student.kt
+++ app/src/main/java/com/example/tutorsnotebook/entities/Student.kt
@@ -6,5 +6,14 @@ class Student(
     var surname: String,
     var studentPhone: Int,
     var parentPhone: Int,
-    var avgScore: Int = 0
-)
+    var avgScore: Int = 0,
+    // to update average score correctly
+    var scoreCounter: Int = 0,
+    // to let app know if score is currently ascending or not
+    var isScoreAscending: Boolean = true
+) {
+    // sample demonstration of adding a score of a new student's mark
+    fun addScore(newScore: Int) {
+        avgScore = (avgScore * scoreCounter + newScore) / ++scoreCounter
+    }
+}
