341a7827c1c422c9508be03f5a21ce51415b50e3
BogoslovskiyNikita <Kartawyh1337@yandex.ru>
2021-06-27 14:59:11 +0300

    changed Question entity

--- app/src/main/java/com/example/tutorsnotebook/entities/Question.kt
+++ app/src/main/java/com/example/tutorsnotebook/entities/Question.kt
@@ -1,6 +1,6 @@
 package com.example.tutorsnotebook.entities
 
-class Question(val problem: String, val answers: List<String>, val rightAnswer: Int) {
+class Question(val problem: String, val answers: MutableList<String>, val rightAnswer: Int) {
     override fun toString(): String {
         return "$problem $answers $rightAnswer"
     }
