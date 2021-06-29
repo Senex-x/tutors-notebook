72d73575163657b1a37f31be7e6c362bacede121
senss <senses.dimit@gmail.com>
2021-06-29 05:35:46 +0300

    add quiz recycler

--- app/src/main/java/com/example/tutorsnotebook/views/fragments/student/TestsFragment.kt
+++ app/src/main/java/com/example/tutorsnotebook/views/fragments/student/TestsFragment.kt
@@ -4,19 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+]import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.tutorsnotebook.R
+import com.example.tutorsnotebook.entities.Quiz
+import com.example.tutorsnotebook.views.adapters.QuizAdapter
+
 
 class TestsFragment : Fragment() {
+    lateinit var adapter: QuizAdapter
+    private var quizList = mutableListOf<Quiz>()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_tests, container, false)
-
-        // Put initializers here
+        setUpViews()
 
         return rootView;
     }
+
+    fun setUpViews() {
+        setUpRecyclerView()
+    }
+
+    private fun setUpRecyclerView() {
+        adapter = QuizAdapter(requireActivity(), quizList)
+        quizRecyclerView.LayoutManager = GridLayoutManager(requireActivity(), 2)
+        quizRecyclerView.adapter = adapter
+    }
+
 }
