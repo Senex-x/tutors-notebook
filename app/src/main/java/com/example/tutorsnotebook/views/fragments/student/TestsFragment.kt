a62c5ab0977dfe70d5aba26d8d426959c99b6014
senss <senses.dimit@gmail.com>
2021-07-01 10:34:46 +0300

    firebase integration

--- app/src/main/java/com/example/tutorsnotebook/views/fragments/student/TestsFragment.kt
+++ app/src/main/java/com/example/tutorsnotebook/views/fragments/student/TestsFragment.kt
@@ -1,21 +1,26 @@
 package com.example.tutorsnotebook.views.fragments.student
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tutorsnotebook.R
 import com.example.tutorsnotebook.entities.Quiz
 import com.example.tutorsnotebook.views.adapters.QuizAdapter
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class TestsFragment : Fragment() {
     lateinit var adapter: QuizAdapter
     private var quizList = mutableListOf<Quiz>()
     private var quizRecuclerView: RecyclerView? = null
+    lateinit var firestore: FirebaseFirestore
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,9 +28,25 @@ class TestsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_tests, container, false)
+        initFireStore()
         initRecyclerView(rootView)
 
         return rootView;
+    }
+
+    private fun initFireStore() {
+        firestore = FirebaseFirestore.getInstance()
+        val collectionReference = firestore.collection("quizzes")
+        collectionReference.addSnapshotListener{ value, error ->
+            if (value == null || error != null) {
+                Toast.makeText(getActivity(), "Error fetching data", Toast.LENGTH_SHORT).show()
+                return@addSnapshotListener
+            }
+            Log.d("DATA", value.toObjects(Quiz::class.java).toString())
+            quizList.clear()
+            quizList.addAll(value.toObjects(Quiz::class.java))
+            adapter.notifyDataSetChanged()
+        }
     }
 
     private fun initRecyclerView(rootView: View) {
